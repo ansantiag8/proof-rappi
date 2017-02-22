924750a4d7d6171745a2caf0e669b7651306916b
Santiago Rodriguez <an.santiago89@gmail.com>
2017-02-21 21:09:12 -0500

    Se realizan validaciones de control las operaciones tienen que ser solo dos, query o update, x y z \y w deben ser numeros, x y \y z no puden ser mayores a n \y se crea la matriz de acuerdo a la n ingresada

--- # README.md
+++ # README.md
@@ -3,3 +3,4 @@
 Pasos
 1. instalación de node
 2. ejecutar npm install
+3. ejecutar node index.js
--- index.js
+++ index.js
@@ -3,9 +3,25 @@ const readline = require('readline')
 let t = 1 // ejecuciones del ejercicio
 let n = 3
 let m = 2 // numero de operaciones
-// let aux1N = n
-// let matrix = []
-// // let matrix = [[[0]], [[0]], [[0]]]
+let aux1N = n
+let matrix = []
+
+let makeMatrix = () => {
+  for (; aux1N >= 0; aux1N--) {
+    let aux2N = n
+
+    matrix.unshift([])
+    for (; aux2N >= 0; aux2N--) {
+      let aux3N = n
+
+      matrix[0].unshift([])
+      for (; aux3N >= 0; aux3N--) {
+        matrix[0][0].unshift(0)
+        // matrix[0][0].unshift(`${aux1N}-${aux2N}-${aux3N}`)
+      }
+    }
+  }
+}
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -42,7 +58,7 @@ let readNM = () => {
 
     if (!isNaN(n) && !isNaN(m)) {
       if ((n >= 1 && n <= 100) && (m >= 1 && m <= 1000)) {
-        // console.log(`${n} - ${m}`)
+        makeMatrix()
         readO()
       } else {
         console.log(`La n debe estar entre 1 y 100 y la m entre 1 y 1000`)
@@ -52,17 +68,50 @@ let readNM = () => {
       console.log(`Digite un número espacio otro número favor`)
       readNM()
     }
-
-    // rl.close()
   })
 }
 
 let readO = () => {
-  // for (; t >= 0; t--) {
-  for (; m > 0; m--) {
-    console.log(`preguntara por las transacciones a realizar`)
-  }
-  // }
+  rl.question('Transacción: ', (answer) => {
+    let data = answer.split(' ')
+    let operation = data[0].toLowerCase()
+
+    switch (operation) {
+      case 'update':
+        if (data.length === 5) {
+          let x = parseInt(data[1], 10)
+          let y = parseInt(data[2], 10)
+          let z = parseInt(data[3], 10)
+          let w = parseInt(data[4], 10)
+
+          if (!isNaN(x) &&
+          !isNaN(y) &&
+          !isNaN(z) &&
+          !isNaN(w)) {
+            if (x <= n && y <= n && z <= n) {
+              matrix[x][y][z] = w
+              console.log(matrix)
+            } else {
+              console.log(`los tres primeros numeros deben de ser menor a n "${n}"`)
+              readO()
+            }
+          } else {
+            console.log(`los datos que ingresaste despues del "${operation}" no son todos numeros, verifica por favor`)
+            readO()
+          }
+        } else {
+          console.log(`Formato no valido Ejemplo: "query x1 y1 z1 W"`)
+          readO()
+        }
+        break
+      case 'query':
+
+        break
+      default:
+        console.log(`La operación "${data[0]}" no es reconocida`)
+        readO()
+    }
+  })
 }
 
 readT()
@@ -70,20 +119,6 @@ readT()
 //
 //
 // for (; t >= 0; t--) {
-//   for (; aux1N >= 0; aux1N--) {
-//     let aux2N = n
-//
-//     matrix.unshift([])
-//     for (; aux2N >= 0; aux2N--) {
-//       let aux3N = n
-//
-//       matrix[0].unshift([])
-//       for (; aux3N >= 0; aux3N--) {
-//         matrix[0][0].unshift(0)
-//         // matrix[0][0].unshift(`${aux1N}-${aux2N}-${aux3N}`)
-//       }
-//     }
-//   }
 //   for (; m >= 0; m--) {
 //   }
 // }
